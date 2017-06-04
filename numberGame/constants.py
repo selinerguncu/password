f38dced1a2c32fe045c6ed05dd3b7f38662c1bb9
Selin Erguncu <erguncu@usc.edu>
2017-06-04 01:17:19 -0700

    added constants

---
+++ numberGame/constants.py
@@ -0,0 +1,13 @@
+onlyNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+onlyLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'y', 'z', 'x', 'q', 'w']
+numbersLowercaseLetters = onlyNumbers + onlyLetters
+uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbersAllLetters = numbersLowercaseLetters + uppercaseLetters
+
+Constants = {
+    "onlyNumbers" : onlyNumbers,
+    "onlyLetters" : onlyLetters,
+    "numbersLowercaseLetters" : numbersLowercaseLetters,
+    "uppercaseLetters" : uppercaseLetters,
+    "numbersAllLetters" : numbersAllLetters
+}
