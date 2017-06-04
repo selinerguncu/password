86b3a803f8a10b3720c1591043f6d064a1932425
Selin Erguncu <erguncu@usc.edu>
2017-06-04 01:27:21 -0700

    used new constants file

--- numberGame/code.py
+++ numberGame/code.py
@@ -1,87 +1,55 @@
 import random
+from numberGame.constants import Constants
 
 class Password:
-	def __init__(self, length, complexity, goldCoins, silverCoins):
-		self.length = int(length)
-		self.complexity = int(complexity)
-		self.goldCoins = int(goldCoins)
-		self.silverCoins = int(silverCoins)
+    def __init__(self, length, complexity, goldCoins, silverCoins):
+        self.length = int(length)
+        self.complexity = int(complexity)
+        self.goldCoins = int(goldCoins)
+        self.silverCoins = int(silverCoins)
 
-	def create(self):
-		self.onlyNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-		self.onlyLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'y', 'z', 'x', 'q', 'w']
-		self.numbersLetters = self.onlyNumbers + self.onlyLetters
+    def create(self):
+        onlyNumbers = Constants["onlyNumbers"]
+        onlyLetters = Constants["onlyLetters"]
+        numbersLetters = Constants["numbersLowercaseLetters"]
+        uppercaseLetters = Constants["uppercaseLetters"]
+        numbersAllLetters = Constants["numbersAllLetters"]
 
-		if self.complexity == 0:
-			password = random.sample(self.onlyNumbers, self.length)
-		elif self.complexity == 1:
-			password = random.sample(self.onlyLetters, self.length)
-		elif self.complexity == 2:
-			password = random.sample(self.numbersLetters, self.length)
-		
-		return password
+        if self.complexity == 0:
+            password = random.sample(onlyNumbers, self.length)
+        elif self.complexity == 1:
+            password = random.sample(onlyLetters, self.length)
+        elif self.complexity == 2:
+            password = random.sample(numbersLetters, self.length)
+        elif self.complexity == 3:
+            password = random.sample(numbersAllLetters, self.length)
+
+        return password
 
 
-	def drawBoxes(self):
-		print ((('*' * 5) + '   ') * self.length)
-		for i in range(3):
-			print ((('*' + ' ' * 3 + '*') + '   ') * self.length)
-		print ((('*' * 5) + '   ') * self.length)
+    def drawBoxes(self):
+        print ((('*' * 5) + '   ') * self.length)
+        for i in range(3):
+            print ((('*' + ' ' * 3 + '*') + '   ') * self.length)
+        print ((('*' * 5) + '   ') * self.length)
 
-	def evaluate(self, password, guess):
 
-		self.goldCoinsReceived = 0
-		self.silverCoinsReceived = 0
+    def evaluate(self, password, guess):
 
-		for i in range(self.length):
-			if guess[i] == password[i]:
-				self.goldCoinsReceived += 1
-			elif guess[i] in password:
-				self.silverCoinsReceived += 1
-		
+        self.goldCoinsReceived = 0
+        self.silverCoinsReceived = 0
 
-		self.evaluation = {}
-		self.evaluation["guess"] = guess
-		self.evaluation["goldCoinsReceived"] = self.goldCoinsReceived
-		self.evaluation["silverCoinsReceived"] = self.silverCoinsReceived 
+        for i in range(self.length):
+            if guess[i] == password[i]:
+                self.goldCoinsReceived += 1
+            elif guess[i] in password:
+                self.silverCoinsReceived += 1
 
-		return self.evaluation
 
-	def update(self, goldCoins, silverCoins, round_, evaluation):
-		guess = evaluation["guess"]
-		# round_ = evaluation["round"]
-		goldCoinsReceived = int(evaluation["goldCoinsReceived"])
-		silverCoinsReceived = int(evaluation["silverCoinsReceived"])
-		round_ = int(round_)
-		self.goldCoinsInBag = int(goldCoins)
-		self.silverCoinsInBag = int(silverCoins)
-		
-		if self.goldCoinsInBag == 0:
-			goldCoinsReceived = '**'
+        self.evaluation = {}
+        self.evaluation["guess"] = guess
+        self.evaluation["goldCoinsReceived"] = self.goldCoinsReceived
+        self.evaluation["silverCoinsReceived"] = self.silverCoinsReceived
 
-		if self.silverCoinsInBag == 0:
-			silverCoinsReceived = '**'
-
-		if goldCoinsReceived != '**':
-			self.goldCoinsInBag = self.goldCoins - goldCoinsReceived
-			if self.goldCoinsInBag <= 0:
-				self.goldCoinsInBag = 0
-
-		if silverCoinsReceived != '**':
-			self.silverCoinsInBag = self.silverCoins - silverCoinsReceived
-			if self.silverCoinsInBag <= 0:
-				self.silverCoinsInBag = 0
-
-		round_ += 1
-
-		updated = {}
-		updated["round"] = round_
-		updated["guess"] = guess
-		updated["goldCoinsReceived"] = goldCoinsReceived
-		updated["silverCoinsReceived"] = silverCoinsReceived
-		updated["goldCoinsInBag"] = self.goldCoinsInBag
-		updated["silverCoinsInBag"] = self.silverCoinsInBag
-
-		return updated
-
+        return self.evaluation
 
